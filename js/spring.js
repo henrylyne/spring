977b68ede2ab59777c60005ede09427f1e0d6736
henrylyne <henry.lyne@urbanairship.com>
2016-01-28 11:09:03 -0800

    Made some UI improvements around highlighting and display.

--- js/spring.js
+++ js/spring.js
@@ -1,5 +1,7 @@
 // On page load
 window.onload = function(e){
+	var high_color = '#5c5';
+	var lo_color = '#ddd';
 	
 	init = function() {
 		// - make tag boxes clickable
@@ -15,18 +17,30 @@ window.onload = function(e){
 	highlightTag = function(ele) {
 		console.log(ele.id);
 		var tagClass = "tag_"+ele.id
-		
-		setBackground(tagClass, '#5c5');
-		if(this.lastTagClass) { setBackground(lastTagClass, '#ddd'); }
-		//var elements = document.getElementsByClassName(tagClass);
-	
-		//for(var i=0; i<elements.length; i++){
-		//	elements[i].setAttribute("style", "background-color: #5c5;");
-		//}
+
+		// turn off the previous highlight
+		if(this.lastTagClass) {
+			this.lastEle.setAttribute("style", "background-color: "+lo_color+";");
+			setBackground(lastTagClass, lo_color);
+		}
+
+		// if a new choice turn on highlight
+		if(tagClass != this.lastTagClass) {
+			ele.setAttribute("style", "background-color: "+high_color+";");
+			setBackground(tagClass, high_color);
+		// clicked on the same tag again, then turn off highlighting for that tag
+		} else {
+			delete this.lastEle;
+			delete this.lastTagClass;
+			return;
+		}
+
+		// remember the last tag chosen
+		this.lastEle = ele
 		this.lastTagClass = tagClass
 	};
 
-	// set the background color fort all elements with a certain class
+	// set the background color for all elements with a certain class
 	setBackground = function(tagClass, color) {
 		var elements = document.getElementsByClassName(tagClass);
 	
